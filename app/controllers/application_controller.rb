a7bb64c7bd87c0636d93f5d5a41b317aefd65108
Jesse Hidalgo <jesse_hidalgo8@yahoo.com>
2022-12-05 10:52:36 -0500

    Updated loading page w/ image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :about])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :about])
   end
+
+  def after_sign_in_path_for(resource)
+    plants_path(current_user) # your path
+  end
 end
